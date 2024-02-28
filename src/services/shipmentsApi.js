54c5ed415a480b624839927bdcd159c93690b109
Sara Ahmad <111248837+sara8ahmad@users.noreply.github.com>
2024-02-28 15:24:59 +0200

    Update shipmentsApi.js

--- src/services/shipmentsApi.js
+++ src/services/shipmentsApi.js
@@ -2,7 +2,16 @@ const API_URL = 'https://tracking.bosta.co/shipments';
 
 
 export async function getShipment(id) {
-
-    return await fetch(`${API_URL}/track/${id}`)
-                .then(res => res.json())
-                .then(data => data)  }
+    try {
+      const response = await fetch(`${API_URL}/track/${id}`);
+      if (!response.ok) {
+        throw new Error('Error fetching shipment data');
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      
+      console.error(error);
+      throw error;
+    }
+  }
